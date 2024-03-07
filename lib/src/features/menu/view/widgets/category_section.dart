92b69f093b690b9e3676f15fc78a7ab7039aeee6
Люда Олесова <flyver33@gmail.com>
2024-03-08 00:07:58 +0600

    added grid

---
+++ lib/src/features/menu/view/widgets/category_section.dart
@@ -0,0 +1,41 @@
+import 'package:effective_coffee/src/features/menu/models/category_model.dart';
+import 'package:effective_coffee/src/features/menu/view/widgets/product_card.dart';
+import 'package:flutter/material.dart';
+
+class CategorySection extends StatelessWidget {
+  final CategoryModel category;
+
+  const CategorySection({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          child: Text(
+            category.categoryName,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+        Flexible(
+          child: GridView.builder(
+            shrinkWrap: true,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: 16.0,
+              crossAxisSpacing: 16.0,
+              mainAxisExtent: 196,
+            ),
+            itemCount: category.products.length,
+            itemBuilder: (context, index) {
+              return ProductCard(product: category.products[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
