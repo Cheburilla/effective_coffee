6c8f8a6d5f4abb59c20c66d4a8cfe1e2d040a542
Alexander Safronov <sanya.safron2003@gmail.com>
2024-04-10 20:55:25 +0600

    added mappers

---
+++ lib/src/features/menu/utils/product_mapper.dart
@@ -0,0 +1,13 @@
+import 'package:effective_coffee/src/features/menu/models/DTOs/product_dto.dart';
+import 'package:effective_coffee/src/features/menu/models/product_model.dart';
+import 'package:effective_coffee/src/features/menu/utils/category_mapper.dart';
+
+extension ProductMapper on ProductDTO {
+  ProductModel toModel() => ProductModel(
+        id: id,
+        name: name,
+        price: price,
+        imagePath: imageUrl,
+        category: category.toModel(),
+      );
+}
