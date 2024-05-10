7727da998d2932f76f256a0be15911242aa6a2ac
Alexander Safronov <sanya.safron2003@gmail.com>
2024-05-10 12:42:24 +0600

    Service was moved from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
 import 'dart:developer';
 import 'package:effective_coffee/firebase_options.dart';
+import 'package:effective_coffee/src/common/local_notification_service.dart';
 import 'package:effective_coffee/src/features/menu/bloc/base_observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:effective_coffee/src/app.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {}
 
@@ -30,43 +30,12 @@ void main() async {
       sound: true,
     );
 
-    const AndroidNotificationChannel channel = AndroidNotificationChannel(
-      'high_importance_channel',
-      'High Importance Notifications',
-      importance: Importance.high,
-    );
-
-    FlutterLocalNotificationsPlugin notificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-
-    await notificationsPlugin.initialize(const InitializationSettings(
-        android: AndroidInitializationSettings('@drawable/app_icon')));
-
-    await notificationsPlugin
-        .resolvePlatformSpecificImplementation<
-            AndroidFlutterLocalNotificationsPlugin>()
-        ?.createNotificationChannel(channel);
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-
-      if (notification != null && android != null) {
-        notificationsPlugin.show(
-            notification.hashCode,
-            notification.title,
-            notification.body,
-            NotificationDetails(
-              android: AndroidNotificationDetails(
-                channel.id,
-                channel.name,
-                icon: '@drawable/app_icon',
-              ),
-            ));
-      }
-    });
-
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    LocalNotificationService.initialize();
+
+    FirebaseMessaging.onMessage
+        .listen((message) => LocalNotificationService.display(message));
 
     runApp(const CoffeeShopApp());
   }, (error, stack) {
